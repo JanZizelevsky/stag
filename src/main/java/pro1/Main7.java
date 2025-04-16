85e124c3bbe58374b5ab3e60f3d4bd60d474d705
JanZizelevsky <janhorak272004@gmail.com>
2025-04-16 20:48:41 +0200

    2

--- src/main/java/pro1/Main7.java
+++ src/main/java/pro1/Main7.java
@@ -1,7 +1,21 @@
 package pro1;
 
-public class Main7 {
-    public static void main(String[] args) {
+import com.google.gson.Gson;
+import pro1.apiDataModel.SpecializationsList;
+import java.util.stream.Collectors;
 
+public class Main7 {    
+    public static void main(String[] args) {
+        System.out.println(specializationDeadlines(2025));
     }
+    public static String specializationDeadlines(int year) {
+        String json = Api.getSpecializations(year);
+        SpecializationsList specializations = new Gson().fromJson(json, SpecializationsList.class);
+        return specializations.items.stream()
+                .map(s -> s.deadline)
+                .sorted()
+                .collect(Collectors.joining(","));
+    }
+
+
 }
---
+++ src/main/java/pro1/apiDataModel/Specialization.java
@@ -0,0 +1,8 @@
+package pro1.apiDataModel;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Specialization {
+    @SerializedName("termin")
+    public String deadline;
+} 
